fa486a2452ff8ab7a8d0a4b178b00669443ecc25
Nisam Git <nisamvp10@gmail.com>
2026-04-01 17:37:01 +0530

    meta header

--- app/Views/frontend/inc/header.php
+++ app/Views/frontend/inc/header.php
@@ -1,7 +1,10 @@
 
 <?= view('frontend/inc/headerLink');?>
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKJ4LSM7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
  <!--Preloader-->
     <div id="preloader">
         <div id="loader" class="loader">
